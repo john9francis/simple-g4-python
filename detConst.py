891aa0c7bd94b4d612b22ef3092a11413d88964a
John Francis <john9francis@gmail.com>
2023-11-04 10:25:52 -0600

    Messing around but still getting the error. I wonder if it's my docker image?

--- detConst.py
+++ detConst.py
@@ -4,6 +4,8 @@ from Geant4 import G4Box
 from Geant4 import G4LogicalVolume
 from Geant4 import G4PVPlacement
 from Geant4 import G4ThreeVector
+from Geant4 import G4RotationMatrix
+from Geant4 import G4Transform3D
 
 from Geant4 import gNistManager
 
@@ -23,12 +25,13 @@ class DetectorConstruction(G4VUserDetectorConstruction):
     # Define world
     solidWorld = G4Box("solidWorld", 10, 10, 10)
     logicWorld = G4LogicalVolume(solidWorld, air, "logicWorld")
-    physWorld = G4PVPlacement(None, G4ThreeVector(0, 0, 0), "physWorld", logicWorld, None, False, 0, False)
 
     # Define a detector
     solidDet = G4Box("solidDet", 1, 1, 1)
     logicDet = G4LogicalVolume(solidDet, gold, "logicDet")
-    G4PVPlacement(None, G4ThreeVector(0,0,0), logicDet, "physDet", logicWorld, False, 0, False)
 
-    return physWorld
+    # place things in the world:
+    physWorld = G4PVPlacement(None, G4ThreeVector(), logicWorld, "physWorld", None, False, 0)
+
+    return logicWorld
     
